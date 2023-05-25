2f9eaa90ab0b3e833b0f3c19a514266769cfe1d5
amirazouaghi <129263715+amirazouaghi@users.noreply.github.com>
2023-05-25 13:43:15 +0000

    added sales model

---
+++ coffee_shop_amira/models/sales.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+WITH sales AS (
+    SELECT
+      order_items.id AS sale_id,
+      date_trunc(orders.created_at, day),
+      orders.customer_id,
+      products.category AS product_category,
+      product_prices.price AS revenue
+FROM {{ source('coffee_shop', 'order_items') }} AS order_items
+LEFT JOIN
+    {{ source('coffee_shop', 'orders') }} AS orders
+LEFT JOIN
+    {{ source('coffee_shop', 'products') }} AS products
+LEFT JOIN
+    {{ source('coffee_shop', 'product_prices') }} AS product_prices
+    ON products.id = product_prices.product_id
+        AND orders.created_at >= product_prices.created_at
+)
+
+SELECT * FROM sales
