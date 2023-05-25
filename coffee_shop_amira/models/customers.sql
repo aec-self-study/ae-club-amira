bee8ba18a03969b47579b1d94cc14e8c565bfc8a
amirazouaghi <129263715+amirazouaghi@users.noreply.github.com>
2023-05-25 12:57:09 +0000

    added tests and sources, edited customers model

--- coffee_shop_amira/models/customers.sql
+++ coffee_shop_amira/models/customers.sql
@@ -1,11 +1,15 @@
 {{ config(materialized='table') }}
 
-SELECT 
+WITH customers AS (
+	SELECT 
 	orders.customer_id AS customer_id,
 	customers.name AS name,
 	customers.email AS email,
 	MIN(orders.created_at) AS first_order_at,
 	COUNT(orders.created_at) AS nr_of_orders
-FROM `analytics-engineers-club.coffee_shop.customers` AS customers
-LEFT JOIN `analytics-engineers-club.coffee_shop.orders` AS orders ON customers.id=orders.customer_id
+FROM {{ source('coffee_shop', 'customers') }} AS customers
+LEFT JOIN {{ source('coffee_shop', 'orders')}} AS orders ON customers.id=orders.customer_id
 GROUP BY 1, 2, 3 
+)
+
+SELECT * FROM customers
